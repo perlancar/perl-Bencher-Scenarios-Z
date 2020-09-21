2da92afd2fb4f609016e0dfc7e1caeb9a56a2efc
perlancar (on pc-home) <perlancar@gmail.com>
2020-09-21 07:12:11 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/Z/Overhead.pm
@@ -0,0 +1,27 @@
+package Bencher::Scenario::Z::Overhead;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Measure startup overhead of Z',
+    modules => {
+        'Z' => {},
+    },
+    code_startup => 1,
+    participants => [
+        {code_template=>'use Z;'},
+        {code_template=>'use Z -modern;'},
+        {code_template=>'use Z -compat;'},
+        {code_template=>'use Z -detect;'},
+    ],
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenarios/Z.pm
@@ -0,0 +1,9 @@
+package Bencher::Scenarios::Z;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Scenarios for benchmarking Z
